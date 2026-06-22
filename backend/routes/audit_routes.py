d7d9bb84674239918b87c35928700dec83ccf816
Vaish1005 <roddavaishnavi10@gmail.com>
2026-06-22 21:40:31 +0530

    saved app.py and audit_routes.py

--- backend/routes/audit_routes.py
+++ backend/routes/audit_routes.py
@@ -5,14 +5,12 @@ router = APIRouter()
 
 audit_logs = []
 
+
 def add_log(message):
 
-    audit_logs.append({
-
-        "message": message,
-        "timestamp": datetime.now().strftime("%H:%M:%S")
-
-    })
+    audit_logs.append(
+        {"message": message, "timestamp": datetime.now().strftime("%H:%M:%S")}
+    )
 
     if len(audit_logs) > 50:
 
