194cee8dfbc941f509794073457f022d812ef4b7
Vaish1005 <roddavaishnavi10@gmail.com>
2026-06-24 19:36:12 +0530

    Replace passlib with bcrypt

--- backend/auth/password_utils.py
+++ backend/auth/password_utils.py
@@ -1,25 +1,13 @@
-from passlib.hash import bcrypt
+import bcrypt
 
 
-def hash_password(password):
+def hash_password(password: str):
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(password.encode("utf-8"), salt)
+    return hashed.decode("utf-8")
 
-    return bcrypt.hash(
-        password
-    )
 
-
-def verify_password(
-
-    plain_password,
-
-    hashed_password
-
-):
-
-    return bcrypt.verify(
-
-        plain_password,
-
-        hashed_password
-
+def verify_password(plain_password: str, hashed_password: str):
+    return bcrypt.checkpw(
+        plain_password.encode("utf-8"), hashed_password.encode("utf-8")
     )
